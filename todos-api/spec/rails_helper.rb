7d417aa021f20aaf5669ba117067aac99ced332a
Akash <aprajapati05@gmail.com>
2018-12-18 15:42:41 +0530

    Authentication added:

--- todos-api/spec/rails_helper.rb
+++ todos-api/spec/rails_helper.rb
@@ -44,7 +44,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include RequestSpecHelper, type: :request
+  # config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -67,7 +69,6 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-
 
   config.include FactoryBot::Syntax::Methods
 
