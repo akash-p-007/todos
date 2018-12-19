9705a430c81b6a6c8a79c244223a84b653de806f
Akash <aprajapati05@gmail.com>
2018-12-19 20:11:05 +0530

    Added Login/Registration Components

---
+++ todoapp/src/components/UploadScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class UploadScreen extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Done</h1>  
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
+export default UploadScreen;
