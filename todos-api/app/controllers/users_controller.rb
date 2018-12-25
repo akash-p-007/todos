8105225428dbc8cf1cfe5e84a2d1e7a2526ded59
Akash <aprajapati05@gmail.com>
2018-12-25 16:00:40 +0530

    Redux setup

--- todos-api/app/controllers/users_controller.rb
+++ todos-api/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params) 
     auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
+    response = { message: Message.account_created, auth_token: auth_token, name: user.name, email: user.email }
     json_response(response, :created)
   end
 
