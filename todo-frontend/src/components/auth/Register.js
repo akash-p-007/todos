64c8cf7f87fe7924f45646817890d30d6c2a3ef6
Akash <aprajapati05@gmail.com>
2018-12-25 15:23:22 +0530

    Added error handling to register component

--- todo-frontend/src/components/auth/Register.js
+++ todo-frontend/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   constructor(){
@@ -25,12 +27,16 @@ class Register extends Component {
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2
-    }
-    console.log(newUser);
+    }; 
+
+    axios.post('http://localhost:3000/signup',newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({errors: err.response.data}));
   }
 
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="register">
         <div className="container">
@@ -40,17 +46,26 @@ class Register extends Component {
               <p className="lead text-center">Create your DevConnector account</p>
               <form onSubmit={this.onSubmit} >
                 <div className="form-group">
-                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name}  onChange={this.onChange}/>
+                  <input type="text" className={classnames("form-control form-control-lg",{
+                    'is-invalid': errors.message
+                  })} placeholder="Name" name="name" value={this.state.name}  onChange={this.onChange}/>
+                  {errors.message && ( <div className="invalid-feedback">{errors.message}</div> )}
                 </div>
                 <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+                  <input type="email"className={classnames("form-control form-control-lg",{
+                    'is-invalid': errors.message
+                  })} placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
                   <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+                  <input type="password" className={classnames("form-control form-control-lg",{
+                    'is-invalid': errors.message
+                  })} placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
+                  <input type="password" className={classnames("form-control form-control-lg",{
+                    'is-invalid': errors.message
+                  })} placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
