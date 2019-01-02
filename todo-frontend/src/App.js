08eb350bccccc84252536e2f22f48f53257f9d9e
Akash <aprajapati05@gmail.com>
2019-01-02 11:55:20 +0530

    Logout after token expiry

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authAction';
+import { setCurrentUser, logoutUser } from './actions/authAction';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -17,6 +17,14 @@ if(localStorage.jwtToken){
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
+
+  //check for expired token
+  const curretTime = Date.now() / 1000;
+  if(decoded.exp < curretTime)
+  {
+    store.dispatch(logoutUser());
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
