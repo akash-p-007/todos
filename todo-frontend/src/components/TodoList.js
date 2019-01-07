6b56ab31af2e05f467099b9670a40980b5c5be25
Akash <aprajapati05@gmail.com>
2019-01-07 17:50:02 +0530

    Updated todos fetching and deletion logic

--- todo-frontend/src/components/TodoList.js
+++ todo-frontend/src/components/TodoList.js
@@ -1,3 +1,82 @@
+// import React, { Component } from "react";
+// import TodoItems from "./TodoItems";
+// import axios from 'axios';
+// import "./TodoList.css";
+
+// class TodoList extends Component {
+//   constructor(props) {
+//     super(props);
+   
+//     this.state = {
+//       items: this.props.todos
+//     };
+//     this.addItem = this.addItem.bind(this);
+//     this.deleteItem = this.deleteItem.bind(this);
+//   }
+
+//   addItem(e) {
+//     if (this._inputElement.value !== "") {
+//       var newItem = {
+//         title: this._inputElement.value,
+//         key: Date.now()
+//       };
+   
+//       // this.setState((prevState) => {
+//       //   return { 
+//       //     items: prevState.items.concat(newItem) 
+//       //   };
+//       // });
+//       axios
+//         .post('http://localhost:3000/todos',newItem)
+//         .then( (res) => {
+//           this.setState({todos: res.data});    
+//         })
+//         .catch(function (error) {
+//         console.log(error);
+//         });
+//       // console.log(this.state.items[0].title);
+//       this._inputElement.value = "";
+//     }        
+//     e.preventDefault();
+//   }
+
+//   deleteItem(key) {
+//     var filteredItems = this.state.items.filter(function (item) {
+//       return (item.key !== key);
+//     });
+   
+//     this.setState({
+//       items: filteredItems
+//     });
+//     // axios.delete('http://localhost:3000/todos',filteredItems)
+//     // .then( (res) => {
+//     //   console.log("deleted"+ res);    
+//     // })
+//     // .catch(function (error) {
+//     // console.log(error);
+//     // });
+//   }
+
+//   render() {
+//     return (
+//       <div className="todoListMain">
+//       <div className="header">
+//         <form onSubmit={this.addItem}>
+//           <input ref={(a) => this._inputElement = a} 
+//                   placeholder="enter task">
+//           </input>
+//           <button type="submit">add</button>
+//         </form>
+//       </div>
+//       <TodoItems entries={this.props.todos}
+//                  delete={this.deleteItem}  />
+//     </div>
+//     );
+//   }
+// }
+ 
+// export default TodoList;
+
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
 import axios from 'axios';
@@ -8,35 +87,64 @@ class TodoList extends Component {
     super(props);
    
     this.state = {
-      items: this.props.todos
+      items: []
     };
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
   }
 
+  componentWillMount(){
+    axios
+      .get('http://localhost:3000/todos')
+      .then( (res) => {
+        for(var i=0;i<res.data.length;i++)
+        {
+          var item = {
+            text: res.data[i].title,
+            key: res.data[i].id
+          };
+          this.setState((prevState) => {
+            return { 
+              items: prevState.items.concat(item) 
+            };
+          });
+        }
+      })
+      .catch(function (error) {
+      console.log(error);
+      });
+  }
+  
+  componentWillReceiveProps(nextProps){
+    //update aaray after adding new prop
+    console.log(nextProps);
+  }
+
   addItem(e) {
+
     if (this._inputElement.value !== "") {
       var newItem = {
         title: this._inputElement.value,
         key: Date.now()
-      };
-   
-      // this.setState((prevState) => {
-      //   return { 
-      //     items: prevState.items.concat(newItem) 
-      //   };
-      // });
-      axios
-        .post('http://localhost:3000/todos',newItem)
-        .then( (res) => {
-          this.setState({todos: res.data});    
-        })
-        .catch(function (error) {
-        console.log(error);
+    };
+    axios
+      .post('http://localhost:3000/todos',newItem)
+      .then( (res) => {
+        // this.setState({todos: res.data});    
+        this.setState((prevState) => {
+          return { 
+            items: prevState.items.concat(newItem) 
+          };
         });
-      // console.log(this.state.items[0].title);
+      })
+      .catch(function (error) {
+      console.log(error);
+      });    
       this._inputElement.value = "";
-    }        
+    }
+     
+    
+    this._inputElement.value = "";           
     e.preventDefault();
   }
 
@@ -44,20 +152,21 @@ class TodoList extends Component {
     var filteredItems = this.state.items.filter(function (item) {
       return (item.key !== key);
     });
-   
+    
     this.setState({
       items: filteredItems
     });
-    // axios.delete('http://localhost:3000/todos',filteredItems)
-    // .then( (res) => {
-    //   console.log("deleted"+ res);    
-    // })
-    // .catch(function (error) {
-    // console.log(error);
-    // });
+    axios
+      .delete(`http://localhost:3000/todos/${key}`,{ data: {id: key}})
+      .then( (res) => {
+        console.log("deleted"+ res);    
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
-  render() {
+  render() { 
     return (
       <div className="todoListMain">
       <div className="header">
